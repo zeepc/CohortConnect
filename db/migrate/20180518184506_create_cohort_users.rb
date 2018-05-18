6fbdc01bdc3e0fabb28f19a4b2dd4651c802824b
David <davidlangva@gmail.com>
2018-05-18 18:03:34 -0400

    hooked up devise with oauth. signing in creates a user and stores all the necessary user  info for each user.

--- db/migrate/20180518184506_create_cohort_users.rb
+++ db/migrate/20180518184506_create_cohort_users.rb
@@ -3,7 +3,7 @@ class CreateCohortUsers < ActiveRecord::Migration[5.2]
     create_table :cohort_users do |t|
       t.integer :cohort_id
       t.integer :user_id
-      t.string :user_role
+      t.string :user_role, default: 'student'
 
       t.timestamps
     end
