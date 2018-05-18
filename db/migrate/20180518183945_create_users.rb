2ae81de399fd0b6d73f66cbb8d97ce726e2885e1
David <davidlangva@gmail.com>
2018-05-18 18:03:34 -0400

    hooked up devise with oauth. signing in creates a user and stores all the necessary user  info for each user.

--- db/migrate/20180518183945_create_users.rb
+++ db/migrate/20180518183945_create_users.rb
@@ -3,12 +3,12 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :profile_image_url
-      t.string :profile_link_url
+      t.string :profile_image_link
+      t.string :profile_link
       t.string :current_employer
       t.string :current_title
-      t.string :city
-      t.string :state
+      t.string :location
+      t.string :summary
 
       t.timestamps
     end
