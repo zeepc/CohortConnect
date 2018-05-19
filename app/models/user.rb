a2a66c8ba2cec31bd5683df8b9c00550fc08ad26
David <davidlangva@gmail.com>
2018-05-19 14:47:15 -0400

    invitable working. not working with oauth though. attempting to integrate by changing omniauth callback controller and devise invitations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.first_name   # assuming the user model has a name
       user.last_name = auth.info.last_name # assuming the user model has an image
