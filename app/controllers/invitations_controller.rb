0252f35de5eacb9ec806f4de73a48c9463256e5a
David <davidlangva@gmail.com>
2018-05-20 00:39:45 -0400

    hooked up email invitations with oauth. sign up works on first account creation from both accepting email invite and from signing up naturally. subsquent logins work for either signup method

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,28 @@
+class InvitationsController < Devise::InvitationsController
+
+  #When the user clicks the email link, they are directed to our website. This controller grabs the inivitation token and stores it in the session for us to process whenever we need it (eg when they return from linked in)
+  # GET /resource/invitation/accept?invitation_token=abcdef
+  def edit
+    if params[:invitation_token] && self.resource = resource_class.find_by_invitation_token(params[:invitation_token], true)
+      session[:invitation_token] = params[:invitation_token]
+      render :edit
+    else
+      set_flash_message(:alert, :invitation_token_invalid)
+      redirect_to after_sign_out_path_for(resource_name)
+    end
+  end
+
+  # PUT /resource/invitation
+  def update
+    self.resource = resource_class.accept_invitation!(params[resource_name])
+
+    if resource.errors.empty?
+      session[:invitation_token] = nil
+      set_flash_message :notice, :updated
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_accept_path_for(resource)
+    else
+      respond_with_navigational(resource){ render :edit }
+    end
+  end
+end
