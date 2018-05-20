c0bb306d5451922ba40484c562d92426daa7da75
David <davidlangva@gmail.com>
2018-05-20 00:39:45 -0400

    hooked up email invitations with oauth. sign up works on first account creation from both accepting email invite and from signing up naturally. subsquent logins work for either signup method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,32 @@ class User < ApplicationRecord
   has_many :cohorts, through: :cohort_users
 
   
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_name   # assuming the user model has a name
-      user.last_name = auth.info.last_name # assuming the user model has an image
-      user.profile_image_link = auth.info.image
-      user.profile_link = auth.info.urls.public_profile
-      user.location = auth.info.location.name
-      user.summary = auth.extra.raw_info.summary
-      user.current_employer = auth.extra.raw_info.positions.values[1][0].company.name
-      user.current_title = auth.extra.raw_info.positions.values[1][0].title
-      # puts "%%%%%%%%"
-      # puts JSON.pretty_generate(auth.extra.raw_info.positions.values[1][0])
-      # puts JSON.pretty_generate(auth)
+  def from_omniauth(auth)
+    
+    self.email = auth.info.email
+    self.provider = auth.provider
+    self.uid = auth.uid
+    self.first_name = auth.info.first_name   # assuming the self model has a name
+    self.last_name = auth.info.last_name # assuming the self model has an image
+    self.profile_image_link = auth.info.image
+    self.profile_link = auth.info.urls.public_profile
+    self.location = auth.info.location.name
+    self.summary = auth.extra.raw_info.summary
+    self.current_employer = auth.extra.raw_info.positions.values[1][0].company.name
+    self.current_title = auth.extra.raw_info.positions.values[1][0].title
 
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-      
-    end
+    if !self.password
+      self.password = Devise.friendly_token[0,20]
+    end 
+
+    
+    # puts "%%%%%%%%"
+    # # puts JSON.pretty_generate(auth.extra.raw_info.positions.values[1][0])
+    # puts JSON.pretty_generate(auth)
+    return self
+
+    # If you are using confirmable and the provider(s) you use validate emails, 
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!  
   end
 end
