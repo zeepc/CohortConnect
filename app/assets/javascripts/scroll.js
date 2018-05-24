8d39b5d49758c8d8f9ec17dfc7b7fede8b4a4d8b
Janelle Ballack <janelle@janelleballack.com>
2018-05-24 10:57:45 -0400

    added sticky_nav.js, updated user cards, updated create cohort sidebar

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,82 @@
+/*!
+
+ =========================================================
+ * Sticky Header-Now-ui-kit 
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/now-ui-kit
+ * Copyright 2017 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md)
+
+ * Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+var transparent = true;
+
+var fixedTop = false;
+
+var navbar_initialized,
+    backgroundOrange = false,
+    toggle_initialized = false;
+
+$(document).ready(function() {
+
+    $navbar = $('.navbar[color-on-scroll]');
+    scroll_distance = $navbar.attr('color-on-scroll') || 500;
+
+    // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
+
+    if ($('.navbar[color-on-scroll]').length != 0) {
+        navBar.checkScrollForTransparentNavbar();
+        $(window).on('scroll', navBar.checkScrollForTransparentNavbar)
+    }
+    });
+
+    navBar = {
+    misc: {
+        navbar_menu_visible: 0
+    },
+
+    checkScrollForTransparentNavbar: debounce(function() {
+        if ($(document).scrollTop() > scroll_distance) {
+            if (transparent) {
+                transparent = false;
+                $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+                $('.navbar[color-on-scroll]').addClass('bg-white');
+            }
+        } else {
+            if (!transparent) {
+                transparent = true;
+                $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+                $('.navbar[color-on-scroll]').removeClass('bg-white');
+            }
+        }
+    }, 17),
+
+}
+
+
+
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    };
+};
