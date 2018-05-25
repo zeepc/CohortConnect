2ede81880a47d85ebafc63671b2570af0263188a
Janelle Ballack <janelle@janelleballack.com>
2018-05-25 14:17:39 -0400

    updated layout styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-$(document).ready(function(){
-  $("#modal_btn").click(function(){
-      $('.modal, .modal-backdrop').hide();
-  });
-})
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,24 +1,7 @@
-/*!
+// Sticky header -- code from now_ui_kit created by www.creative-tim.com
 
- =========================================================
- * Sticky Header-Now-ui-kit 
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/now-ui-kit
- * Copyright 2017 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
 var transparent = true;
-
 var fixedTop = false;
-
 var navbar_initialized,
     backgroundOrange = false,
     toggle_initialized = false;
@@ -57,15 +40,7 @@ $(document).ready(function() {
 
 }
 
-
-
-  
-
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
+// Returns a function, that, as long as it continues to be invoked, will not be triggered. The function will be called after it stops being called for N milliseconds. If `immediate` is passed, trigger the function on the leading edge, instead of the trailing.
 
 function debounce(func, wait, immediate) {
     var timeout;
@@ -80,3 +55,12 @@ function debounce(func, wait, immediate) {
         if (immediate && !timeout) func.apply(context, args);
     };
 };
+
+// Modal close on submit
+
+$(document).ready(function(){
+    $("#modal_btn").click(function(){
+        $('.modal, .modal-backdrop').hide();
+    });
+  })
+  
