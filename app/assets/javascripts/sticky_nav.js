b32ed45d3f309234bcd41503bd8a429cea9f6442
Janelle Ballack <janelle@janelleballack.com>
2018-05-25 10:36:22 -0400

    modal close on submit and admin buttons

--- app/assets/javascripts/sticky_nav.js
+++ app/assets/javascripts/sticky_nav.js
@@ -59,6 +59,8 @@ $(document).ready(function() {
 
 
 
+  
+
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
